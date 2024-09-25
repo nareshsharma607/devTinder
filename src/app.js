eeb903552fb39aeb495eff73cdff47003b792dd0
naresh sharma <nareshsharma@nareshs-MacBook-Air.local>
2024-09-25 15:07:33 +0530

    add api and schema validation

--- src/app.js
+++ src/app.js
@@ -9,16 +9,49 @@ app.post('/signup',async(req,res)=>{
     console.log(req.body)
     const userObj=req.body
     try {
+        console.log(User)
         const user=new User(userObj)
+        console.log(user)
         await user.save()
         res.send("User added succesfully")
     } catch (error) {
-        res.status(400).send("Error in saving the user",error)
-    }
+        res.status(400).send("Error in saving the user "+ error)
+    } 
   
 })
 
 
+app.get("/feed",async(req,res)=>{
+    try {
+        const users=await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(400).send("Error in getting the users",error)
+    }
+})
+
+app.delete('/user',async(req,res)=>{
+    const id=req.body.id
+    try {
+        const user=await User.findByIdAndDelete(id)
+        res.send("user deleted succesfully")
+    } catch (error) {
+        res.send("error in deleting the user")
+
+
+    }
+})
+
+
+app.patch('/user',async(req,res)=>{
+    const id=req.body.id
+    try {
+        const user=await User.findByIdAndUpdate({_id:id},req.body,{runValidators:true})
+        res.send("user updated succesfully")
+    } catch (error) {
+        res.send("error in updating the user")
+    }
+})
 
 connectDB().then(()=>{
     console.log("connected to db succesfully")
--- src/model/user.js
+++ src/model/user.js
@@ -2,24 +2,52 @@ const mongoose=require("mongoose")
 
 const userSchema=new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:3,
+        maxLegth:50
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        lowercase:true,
+        required:true,
+        unique:true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
     },
     gender:{
-        type:String
+        type:String,
+        lowercase:true
+        ,
+        validate(value){
+        if(!["male",'female','other'].includes(value)){
+            throw Error("Invalid gender")
+        }
+        }
+    },
+    photoUrl:{
+     type:String,
+    default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vectors%2Fblank-profile-picture-vectors&psig=AOvVaw28_fEncl1Lj4Abk5W_a1b9&ust=1727340594388000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOi0pKfb3YgDFQAAAAAdAAAAABAE"
+    },
+    about:{
+        type:String,
+        default:"asdadsaasddsfgfgerf"
+    },
+    skills:{
+        type:[String],
+        
     }
-})
+},{timestamps:true})
 
 const userModel=mongoose.model("User",userSchema)
 
